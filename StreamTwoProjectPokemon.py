72e96b868ed4ba423291c12eed425acde88eb6b5
Zaniron <tristan.challis@live.co.uk>
2018-02-11 21:54:46 +0000

    csv hosted to mongoDB, starting setup

--- StreamTwoProjectPokemon.py
+++ StreamTwoProjectPokemon.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask import render_template
+from pymongo import MongoClient
+import json
 
 app = Flask(__name__)
+
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+DBS_NAME = 'pokemonData'
+COLLECTION_NAME = 'pokemon'
 
 
 @app.route('/')
