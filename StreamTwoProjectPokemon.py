bd89880b8e9afc86f3808f981eb6e5237254cc23
Zaniron <tristan.challis@live.co.uk>
2018-02-12 19:02:08 +0000

    connecting hosted data to project

--- StreamTwoProjectPokemon.py
+++ StreamTwoProjectPokemon.py
@@ -12,9 +12,51 @@ COLLECTION_NAME = 'pokemon'
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    """
+    Flask view to serve the first dashboard page
+    """
+    return render_template("index.html")
+
+
+@app.route('/teambuilder')
+def teambuilder():
+    """
+    Flask view to serve the team building page
+    """
+    return render_template("teambuilder.html")
+
+
+@app.route("/pokemonData/pokemon")
+def pokemon_data():
+    """
+    Flask view to serve the pokemon data from MongoDB
+    in JSON format
+    """
+
+    # a constant that defines the record fields we want to get
+    FIELDS = {
+        '_id': False,
+        'id': True,
+        'Name': True,
+        'Type 1': True,
+        'Type 2': True,
+        'Total': True,
+        'HP': True,
+        'Attack': True,
+        'Defense': True,
+        'Sp Atk': True,
+        'Sp Def': True,
+        'Speed': True,
+        'Generation': True,
+        'Legendary': True
+    }
+
+    with MongoClient(MONGO_HOST, MONGO_PORT) as conn:
+        collection = conn[DBS_NAME][COLLECTION_NAME]
+        pokemon = collection.find(projection=FIELDS, limit=1000)
+        return json.dumps(list(pokemon))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
